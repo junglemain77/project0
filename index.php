b6e628b3d61fcd90b575595008c41852423944aa
Val <valenios44@gmail.com>
2023-12-31 22:35:13 +0700

    Minor Change

--- index.php
+++ index.php
@@ -19,10 +19,19 @@ if (!$koneksi) {
   <title>Document</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <style>
+    .mx-auto {
+      width: 1000px
+    }
+
+    .card {
+      margin-top: 15px
+    }
+  </style>
 </head>
 
 <body>
-  <div class="max-auto">
+  <div class="mx-auto">
     <!-- Input Data -->
     <div class="card">
       <div class="card-header">
@@ -30,14 +39,24 @@ if (!$koneksi) {
       </div>
       <div class="card-body">
         <form action="" method="POST">
-
+          <div class="mb-3 row">
+            <label for="nim" class="col-sm-2 col-form-label">NIM</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control-plaintext" id="nim" name="nim" value="<?php echo $nim?>">
+            </div>
+            <div class="mb-3 row">
+            <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control-plaintext" id="nama" name="nama" value="<?php echo $nama?>">
+            </div>
+          </div>
         </form>
       </div>
     </div>
 
     <!-- Get Data -->
     <div class="card">
-      <div class="card-header">
+      <div class="card-header text-white bg-secondary">
         Data Mahasiswa
       </div>
       <div class="card-body">
