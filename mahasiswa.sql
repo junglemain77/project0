1864593495a7cffe94bd0d1de73ea928290ede53
Val <valenios44@gmail.com>
2024-01-01 22:25:00 +0700

    Perubahan Database

--- mahasiswa.sql
+++ mahasiswa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2023 at 04:10 PM
+-- Generation Time: Jan 01, 2024 at 04:24 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -28,21 +28,43 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `mahasiswa` (
-  `id` int(60) NOT NULL,
-  `nama` varchar(200) NOT NULL,
-  `jenis_kelamin` varchar(60) NOT NULL
+  `id` int(11) NOT NULL,
+  `nim` varchar(25) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `alamat` varchar(25) NOT NULL,
+  `fakultas` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `nim`
+-- Dumping data for table `mahasiswa`
 --
 
-CREATE TABLE `nim` (
-  `no` int(60) NOT NULL,
-  `nim` int(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `alamat`, `fakultas`) VALUES
+(4, '20200003', 'Tri Untoro', 'Semarang Tengah', 'soshum'),
+(5, '20200004', 'Ahmad', 'Yogyakarta', 'saintek'),
+(6, '20200005', 'Doni', 'Jakarta', 'soshum'),
+(7, '20200006', 'Budi', 'Klaten', 'soshum');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nim` (`nim`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
